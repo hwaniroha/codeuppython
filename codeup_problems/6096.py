7952b57623dd3c9c0959066f7a79984a2411e6e3
hwaniroha <167195848+hwaniroha@users.noreply.github.com>
2024-10-01 14:26:59 +0000

    2024/10/1 - 23 : 26

--- codeup_problems/6096.py
+++ codeup_problems/6096.py
@@ -1,24 +1,18 @@
-h, w = input().split()
-h = int(h)
-w = int(w)
-
-grid = [[0] * w for _ in range(h)]
-
+board = [list(input().split()) for _ in range(19)]
 n = int(input())
 
 for _ in range(n):
-    l, d, x, y = input().split()
-    l = int(l)
-    d = int(d)
+    x, y = input().split()
     x = int(x) - 1
     y = int(y) - 1
 
-    if d == 0:
-        for i in range(l):
-            grid[x][y + i] = 1
-    else:
-        for i in range(l):
-            grid[x + i][y] = 1
+    for i in range(19):
+        if i != y:
+            board[x][i] = '1' if board[x][i] == '0' else '0'
 
-for row in grid:
-    print(' '.join(str(cell) for cell in row))
+    for i in range(19):
+        if i != x:
+            board[i][y] = '1' if board[i][y] == '0' else '0'
+
+for row in board:
+    print(' '.join(row))
